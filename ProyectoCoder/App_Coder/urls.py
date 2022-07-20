fab1bb75a6da812c8550ccc9e8bbc6af16447f87
Nicolás Martinez <nmartinez@DTVPAN.COM>
2022-07-20 17:39:44 -0300

    Se actualiza el formulario padre, clase playground avanzado 2

--- ProyectoCoder/App_Coder/urls.py
+++ ProyectoCoder/App_Coder/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 
-from App_Coder.views import curso, lista_curso
-from App_Coder.views import cursos, inicio, profesores, estudiantes, entregables
+from App_Coder.views import curso, lista_curso, cursos, inicio, profesores, estudiantes, entregables
 
 urlpatterns = [
     path('agrega-curso/<nombre>/<camada>', curso),
     path('lista-curso/', lista_curso),
-    path('', inicio),
-    path('cursos/', cursos),
-    path('profesores/', profesores),
-    path('estudiantes/', estudiantes),
-    path('entregables/', entregables),
+    path('', inicio, name="Inicio"),
+    path('cursos/', cursos, name="Cursos"),
+    path('profesores/', profesores, name="Profesores"),
+    path('estudiantes/', estudiantes, name="Estudiantes"),
+    path('entregables/', entregables, name="Entregables"),
 ]
